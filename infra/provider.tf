36d4cfafc4024dee086cebd20306064185cc8148
arezoo <93465403+arezoomohammadi22@users.noreply.github.com>
2023-10-09 17:23:14 +0330

    change provider.tf

--- infra/provider.tf
+++ infra/provider.tf
@@ -1,12 +1,18 @@
-provider "azurerm" {
-  features {}
+# Define provider for your local server (e.g., using SSH)
+provider "ssh" {
+  host = "arezoo-mohammadi-nl-rg2.maxtld.dev:2233"  # Update with your server's IP or hostname
+  user = "arezoo"  # Update with your SSH username
+  private_key = file("~/.ssh/id_rsa")  # Update with your SSH private key path
 }
 
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "3.3.0"
-    }
+# Provision resources for your Kubernetes cluster (e.g., VMs, networking)
+resource "null_resource" "kubernetes_cluster" {
+  # Use provisioners to run scripts for setting up Kubernetes on your server
+  triggers = {
+    always_run = "${timestamp()}"
+  }
+
+  provisioner "local-exec" {
+    command = "scripts/setup-k8s.sh"  # Replace with your setup script
   }
 }
